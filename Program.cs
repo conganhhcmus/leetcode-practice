0bd93b786935223c28e8f9211d9b4e8bee608df7
conganhhcmus <conganhhcmus@gmail.com>
2024-09-23 00:32:26 +0700

    feat: add solution problem 440

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     private static void Main(string[] args)
     {
-        Problem_386.Solution.Execute();
+        Problem_440.Solution.Execute();
 
         //Contest_413_Q1.Solution.Execute();
         //Contest_413_Q2.Solution.Execute();
---
+++ problems/440/Solution.cs
@@ -0,0 +1,66 @@
+namespace Problem_440;
+
+public class Solution
+{
+    public static void Execute()
+    {
+        var solution = new Solution();
+        //int n = 957747794, k = 424238336;
+        int n = 10, k = 3;
+        Console.WriteLine(solution.FindKthNumber(n, k));
+        Console.WriteLine(solution.FindKthNumber2(n, k));
+    }
+    public int FindKthNumber(int n, int k)
+    {
+        int curr = 1;
+        k--;
+
+        while (k > 0)
+        {
+            int step = CountSteps(n, curr, curr + 1);
+            if (step <= k)
+            {
+                curr++;
+                k -= step;
+            }
+            else
+            {
+                curr *= 10;
+                k--;
+            }
+        }
+
+        return curr;
+    }
+
+    private int CountSteps(int n, long prefix1, long prefix2)
+    {
+        int steps = 0;
+        while (prefix1 <= n)
+        {
+            steps += int.Min((int)(n + 1 - prefix1), (int)(prefix2 - prefix1));
+            prefix1 *= 10;
+            prefix2 *= 10;
+        }
+        return steps;
+    }
+
+    public int FindKthNumber2(int n, int k)
+    {
+        long currentNum = 1;
+        for (int i = 1; i < k; i++)
+        {
+            if (currentNum * 10 <= n) currentNum *= 10;
+            else
+            {
+                while (currentNum % 10 == 9 || currentNum >= n)
+                {
+                    currentNum /= 10;
+                }
+                currentNum++;
+            }
+        }
+
+        return (int)currentNum;
+    }
+}
