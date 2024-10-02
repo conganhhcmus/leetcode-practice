3965008efd247943fef8c90394f046a090bf087f
conganhhcmus <conganhhcmus@gmail.com>
2024-10-02 17:59:59 +0700

    feat: add solution problem 1331

---
+++ problems/1331/Solution.cs
@@ -0,0 +1,26 @@
+using System.Collections.Immutable;
+
+namespace Problem_1331;
+
+public class Solution
+{
+    public static void Execute()
+    {
+        var solution = new Solution();
+        int[] arr = [37, 12, 28, 9, 100, 56, 80, 5, 12];
+        Console.WriteLine(string.Join(",", solution.ArrayRankTransform(arr)));
+    }
+    public int[] ArrayRankTransform(int[] arr)
+    {
+        int[] ans = new int[arr.Length];
+        int[] tmp = [.. arr.ToHashSet()];
+        Array.Sort(tmp);
+
+        for (int i = 0; i < ans.Length; i++)
+        {
+            int rank = Array.IndexOf(tmp, arr[i]);
+            ans[i] = rank + 1;
+        }
+        return ans;
+    }
+}
