cbaaeb27f23e2dca0f754c0f5c6408e1015c9b7b
conganhhcmus <conganhhcmus@gmail.com>
2024-10-03 21:44:57 +0700

    feat: add solution problem 1456

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     private static void Main(string[] args)
     {
-        Problem_1590.Solution.Execute();
+        Problem_1456.Solution.Execute();
 
         //Contest_413_Q1.Solution.Execute();
         //Contest_413_Q2.Solution.Execute();
---
+++ problems/1456/Solution.cs
@@ -0,0 +1,30 @@
+namespace Problem_1456;
+
+public class Solution
+{
+    public static void Execute()
+    {
+        var s = "abciiidef";
+        var k = 3;
+        var solution = new Solution();
+        Console.WriteLine(solution.MaxVowels(s, k));
+    }
+    public int MaxVowels(string s, int k)
+    {
+        int[] dp = new int[s.Length + 1];
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            dp[i + 1] = dp[i] + ((s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') ? 1 : 0);
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i + k <= s.Length; i++)
+        {
+            ans = Math.Max(ans, dp[i + k] - dp[i]);
+        }
+
+        return ans;
+    }
+}
