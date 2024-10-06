7f97ea5f4c2130fa236860c3bf35745bf5a73011
conganhhcmus <conganhhcmus@gmail.com>
2024-10-06 20:23:34 +0700

    feat: add solution problem 1657

--- README.md
+++ README.md
@@ -65,6 +65,7 @@ All the contest/problem solutions on LeetCode that I attended.
 - [1497. Check If Array Pairs Are Divisible by k](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/1497/Solution.cs)
 - [1514. Path with Maximum Probability](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/1514/Solution.cs)
 - [1590. Make Sum Divisible by P](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/1590/Solution.cs)
+- [1657. Determine if Two Strings Are Close](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/1657/Solution.cs)
 - [1679. Max Number of K-Sum Pairs](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/1679/Solution.cs)
 - [1684. Count the Number of Consistent Strings](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/1684/Solution.cs)
 - [1700. Number of Students Unable to Eat Lunch](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/1700/Solution.cs)
---
+++ problems/1657/Solution.cs
@@ -0,0 +1,41 @@
+namespace Problem_1657;
+
+public class Solution
+{
+    public static void Execute()
+    {
+        string word1 = "abbzzca";
+        string word2 = "babzzcz";
+        var solution = new Solution();
+        Console.WriteLine(solution.CloseStrings(word1, word2));
+    }
+    public bool CloseStrings(string word1, string word2)
+    {
+        if (word1.Length != word2.Length) return false;
+
+        var charCount1 = new int[26];
+        var charCount2 = new int[26];
+        for (int i = 0; i < word1.Length; i++)
+        {
+            charCount1[word1[i] - 'a']++;
+            charCount2[word2[i] - 'a']++;
+        }
+
+        for (int i = 0; i < charCount1.Length; i++)
+        {
+            if ((charCount1[i] == 0 && charCount2[i] != 0)
+            || (charCount1[i] != 0 && charCount2[i] == 0))
+                return false;
+        }
+
+        Array.Sort(charCount1);
+        Array.Sort(charCount2);
+
+        for (int i = 0; i < charCount1.Length; i++)
+        {
+            if (charCount1[i] != charCount2[i]) return false;
+        }
+
+        return true;
+    }
+}
