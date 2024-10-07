b55b5f0ca83ea80d0d7ef04c4352ac3c65b4ea0e
conganhhcmus <conganhhcmus@gmail.com>
2024-10-07 18:00:17 +0700

    feat: add solution problem 394

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     private static void Main(string[] args)
     {
-        Problem_735.Solution.Execute();
+        Problem_394.Solution.Execute();
 
         //Contest_413_Q1.Solution.Execute();
         //Contest_413_Q2.Solution.Execute();
---
+++ problems/394/Solution.cs
@@ -0,0 +1,47 @@
+using System.Text;
+
+namespace Problem_394;
+
+public class Solution
+{
+    public static void Execute()
+    {
+        var solution = new Solution();
+        string encodedString = "2[abc]3[cd]ef";
+        Console.WriteLine(solution.DecodeString(encodedString));
+    }
+    public string DecodeString(string s)
+    {
+        Stack<int> repeatNum = new();
+        Stack<string> result = new();
+        int num = 0;
+        foreach (char c in s)
+        {
+            if (c >= '0' && c <= '9')
+            {
+                num = num * 10 + (c - '0');
+            }
+            else if (c == ']')
+            {
+                string tmp = "";
+                while (result.Peek() != "[")
+                {
+                    tmp = result.Pop() + tmp;
+                }
+                result.Pop(); // remove '['
+                result.Push(string.Concat(Enumerable.Repeat(tmp, repeatNum.Pop())));
+            }
+            else
+            {
+                if (num > 0)
+                {
+                    repeatNum.Push(num);
+                    num = 0;
+                }
+                result.Push(c.ToString());
+            }
+        }
+
+        return string.Join("", result.Reverse());
+    }
+}
