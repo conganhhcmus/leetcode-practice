ed8d3e33a470583d37a80879dfcd584cab2cc3eb
conganhhcmus <conganhhcmus@gmail.com>
2024-10-07 17:44:20 +0700

    feat: add solution problem 735

--- README.md
+++ README.md
@@ -50,6 +50,7 @@ All the contest/problem solutions on LeetCode that I attended.
 - [725. Split Linked List in Parts](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/725/Solution.cs)
 - [729. My Calendar I](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/729/Solution.cs)
 - [731. My Calendar II](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/731/Solution.cs)
+- [735. Asteroid Collision](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/735/Solution.cs)
 - [874. Walking Robot Simulation](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/874/Solution.cs)
 - [884. Uncommon Words from Two Sentences](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/884/Solution.cs)
 - [1004. Max Consecutive Ones III](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/1004/Solution.cs)
---
+++ problems/735/Solution.cs
@@ -0,0 +1,47 @@
+namespace Problem_735;
+
+public class Solution
+{
+    public static void Execute()
+    {
+        var solution = new Solution();
+        int[] asteroids = [-2, -2, 1, -2];
+        var res = solution.AsteroidCollision(asteroids);
+        Console.WriteLine($"[{string.Join(",", res)}]");
+    }
+    public int[] AsteroidCollision(int[] asteroids)
+    {
+        Stack<int> stack = new();
+
+        foreach (int asteroid in asteroids)
+        {
+            bool isExplode = false;
+
+            while (stack.Count > 0 && stack.Peek() > 0 && asteroid < 0)
+            {
+                if (Math.Abs(stack.Peek()) > Math.Abs(asteroid))
+                {
+                    isExplode = true;
+                    break;
+                }
+                else if (Math.Abs(stack.Peek()) == Math.Abs(asteroid))
+                {
+                    stack.Pop();
+                    isExplode = true;
+                    break;
+                }
+                else
+                {
+                    stack.Pop();
+                }
+            }
+
+            if (!isExplode)
+            {
+                stack.Push(asteroid);
+            }
+        }
+
+        return stack.Reverse().ToArray();
+    }
+}
