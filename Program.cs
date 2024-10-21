378e0d26dd1879f38797d13068328f522f438736
conganhhcmus <conganhhcmus@gmail.com>
2024-10-21 17:50:24 +0700

    feat: add solution problem 1593

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     private static void Main(string[] args)
     {
-        Problem_1106.Solution.Execute();
+        Problem_1593.Solution.Execute();
 
         //Contest_413_Q1.Solution.Execute();
         //Contest_413_Q2.Solution.Execute();
---
+++ problems/1593/Solution.cs
@@ -0,0 +1,39 @@
+namespace Problem_1593;
+
+public class Solution
+{
+    public static void Execute()
+    {
+        string s = "wwwzfvedwfvhsww";
+        var solution = new Solution();
+        Console.WriteLine(solution.MaxUniqueSplit(s));
+    }
+    public int MaxUniqueSplit(string s)
+    {
+        HashSet<string> splits = [];
+        int maxSplits = 0;
+
+        void BackTracking(string s)
+        {
+            if (s.Length == 0)
+            {
+                maxSplits = Math.Max(maxSplits, splits.Count);
+                return;
+            }
+
+            for (int i = 1; i <= s.Length; i++)
+            {
+                string prefix = s[..i];
+                if (splits.Add(prefix))
+                {
+                    BackTracking(s[i..]);
+                    splits.Remove(prefix);
+                }
+            }
+        }
+
+        BackTracking(s);
+
+        return maxSplits;
+    }
+}
