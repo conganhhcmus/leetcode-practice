4bee33f839bec8907a26317a68d9f9f407e129c7
conganhhcmus <conganhhcmus@gmail.com>
2024-10-26 17:04:00 +0700

    feat: add solution problem 2458

--- README.md
+++ README.md
@@ -120,6 +120,7 @@ All the contest/problem solutions on LeetCode that I attended.
 - [2406. Divide Intervals Into Minimum Number of Groups](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/2406/Solution.cs)
 - [2416. Sum of Prefix Scores of Strings](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/2416/Solution.cs)
 - [2419. Longest Subarray With Maximum Bitwise AND](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/2419/Solution.cs)
+- [2458. Height of Binary Tree After Subtree Removal Queries](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/2458/Solution.cs)
 - [2491. Divide Players Into Teams of Equal Skill](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/2491/Solution.cs)
 - [2530. Maximal Score After Applying K Operations](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/2530/Solution.cs)
 - [2583. Kth Largest Sum in a Binary Tree](https://github.com/conganhhcmus/leetcode-practice/blob/main/problems/2583/Solution.cs)
---
+++ problems/2458/Solution.cs
@@ -0,0 +1,51 @@
+namespace Problem_2458;
+
+using Helpers.TreeNode;
+
+public class Solution
+{
+    public static void Execute()
+    {
+        var root = TreeNodeHelper.CreateTreeFromArray([1, null, 5, 3, null, 2, 4]);
+        int[] queries = [3, 5, 4, 2, 4];
+        var solution = new Solution();
+        Console.WriteLine($"[{string.Join(",", solution.TreeQueries(root, queries))}]");
+    }
+    public int[] TreeQueries(TreeNode root, int[] queries)
+    {
+        int[] levelArr = new int[100_001];
+        int[] depth = new int[100_001];
+        int[] max1 = new int[100_001];
+        int[] max2 = new int[100_001];
+
+        int DFS(TreeNode root, int level)
+        {
+            if (root is null) return 0;
+            levelArr[root.val] = level;
+            depth[root.val] = 1 + Math.Max(DFS(root.right, level + 1), DFS(root.left, level + 1));
+
+            if (max1[level] < depth[root.val])
+            {
+                max2[level] = max1[level];
+                max1[level] = depth[root.val];
+            }
+            else if (max2[level] < depth[root.val])
+            {
+                max2[level] = depth[root.val];
+            }
+
+            return depth[root.val];
+        }
+
+        DFS(root, 0);
+
+        int[] ans = new int[queries.Length];
+        for (int i = 0; i < ans.Length; i++)
+        {
+            int level = levelArr[queries[i]];
+            ans[i] = (max1[level] == depth[queries[i]] ? max2[level] : max1[level]) + level - 1;
+        }
+
+        return ans;
+    }
+}
