027b954ed78852ed86905da75c08d766aa23e43f
conganhhcmus <conganhhcmus@gmail.com>
2024-11-12 18:01:32 +0700

    feat: add solution problem 2070

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     private static void Main(string[] args)
     {
-        Problem_72.Solution.Execute();
+        Problem_2070.Solution.Execute();
 
         //Contest_413_Q1.Solution.Execute();
         //Contest_413_Q2.Solution.Execute();
---
+++ problems/2070/Solution.cs
@@ -0,0 +1,55 @@
+namespace Problem_2070;
+
+public class Solution
+{
+    public static void Execute()
+    {
+        int[][] items = [[10, 1000]];
+        int[] queries = [5];
+        var solution = new Solution();
+        Console.WriteLine($"[{string.Join(",", solution.MaximumBeauty(items, queries))}]");
+    }
+
+    public int[] MaximumBeauty(int[][] items, int[] queries)
+    {
+        Array.Sort(items, (a, b) => a[0] - b[0]);
+        int n = items.Length;
+        int[] MaxBeauty = new int[n];
+        int max = 0;
+        for (int i = 0; i < n; i++)
+        {
+            max = Math.Max(max, items[i][1]);
+            MaxBeauty[i] = max;
+        }
+        int[] ans = new int[queries.Length];
+        for (int i = 0; i < queries.Length; i++)
+        {
+            int index = FindIndex(items, n, queries[i]);
+            if (index != -1)
+            {
+                ans[i] = MaxBeauty[index];
+            }
+        }
+        return ans;
+    }
+
+    private int FindIndex(int[][] items, int n, int price)
+    {
+        int start = 0;
+        int end = n - 1;
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+
+            if (items[mid][0] > price)
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        return end;
+    }
+}
