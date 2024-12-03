69f3a23d7b1f10e425d21bb6d82b3f96af70c05e
conganhhcmus <conganhhcmus@gmail.com>
2024-12-03 11:16:43 +0700

    feat: add solution problem 2109

---
+++ problems/2109/Solution.cs
@@ -0,0 +1,31 @@
+namespace Problems_2109;
+
+using System.Text;
+
+public class Solution
+{
+    public string AddSpaces(string s, int[] spaces)
+    {
+        StringBuilder sb = new StringBuilder();
+        int idx1 = 0;
+        int idx2 = 0;
+        while (idx1 < s.Length && idx2 < spaces.Length)
+        {
+            if (idx1 == spaces[idx2])
+            {
+                sb.Append(" ");
+                idx2++;
+            }
+            sb.Append(s[idx1]);
+            idx1++;
+        }
+
+        while (idx1 < s.Length)
+        {
+            sb.Append(s[idx1]);
+            idx1++;
+        }
+
+        return sb.ToString();
+    }
+}
