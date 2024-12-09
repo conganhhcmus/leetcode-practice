b980a888403a9b5583c9e9044a11418cd8ad60ee
conganhhcmus <conganhhcmus@gmail.com>
2024-12-09 11:18:36 +0700

    feat: add solution problem 42

---
+++ problems/42/Solution.cs
@@ -0,0 +1,43 @@
+#if DEBUG
+namespace Problems_42;
+#endif
+
+public class Solution
+{
+    public int Trap(int[] height)
+    {
+        int n = height.Length;
+        int ans = 0;
+        int left = 0, right = n - 1;
+        int maxLeft = 0, maxRight = 0;
+        // ans[i] = max(min(maxLeft[i], maxRight[i])-height[i], 0);
+        while (left <= right)
+        {
+            if (height[left] <= height[right])
+            {
+                if (height[left] >= maxLeft)
+                {
+                    maxLeft = height[left];
+                }
+                else
+                {
+                    ans += maxLeft - height[left];
+                }
+                left++;
+            }
+            else
+            {
+                if (height[right] >= maxRight)
+                {
+                    maxRight = height[right];
+                }
+                else
+                {
+                    ans += maxRight - height[right];
+                }
+                right--;
+            }
+        }
+        return ans;
+    }
+}
