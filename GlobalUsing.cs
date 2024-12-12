b6cbb473cdcfb22cbeae9a3a8e5af4cff5922461
conganhhcmus <conganhhcmus@gmail.com>
2024-12-12 11:27:05 +0700

    feat: add solution problem 2558

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -1,4 +1,4 @@
-global using Running = Problems_12;
+global using Running = Problems_2558;
 global using Helpers;
 global using Structures;
 global using Newtonsoft.Json;
---
+++ problems/2558/Solution.cs
@@ -0,0 +1,28 @@
+#if DEBUG
+namespace Problems_2558;
+#endif
+
+public class Solution
+{
+    public long PickGifts(int[] gifts, int k)
+    {
+        long ans = 0;
+        PriorityQueue<int, int> queue = new(Comparer<int>.Create((a, b) => b - a));
+        foreach (int gift in gifts)
+        {
+            queue.Enqueue(gift, gift);
+            ans += gift;
+        }
+
+        while (k > 0)
+        {
+            int gift = queue.Dequeue();
+            int remaining = (int)Math.Sqrt(gift);
+            queue.Enqueue(remaining, remaining);
+            ans -= gift - remaining;
+            k--;
+        }
+
+        return ans;
+    }
+}
