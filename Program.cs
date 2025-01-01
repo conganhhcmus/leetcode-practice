7c3f485e9121d57be40751ff13f0cae9dbc5ba89
conganhhcmus <conganhhcmus@gmail.com>
2025-01-02 00:21:47 +0700

    feat: enhance check answer

--- Program.cs
+++ Program.cs
@@ -38,10 +38,11 @@
         object classInstance = Activator.CreateInstance(typeof(Running.Solution), null);
         ArgumentNullException.ThrowIfNull(classInstance, nameof(classInstance));
 
-        List<string> data = Input();
-        int loop = data.Count / parameters.Length;
-        int index = 0;
-        List<(string output, long executeTime)> lines = [];
+        List<string> testcases = GetTestcase();
+        int loop = testcases.Count / parameters.Length, index = 0;
+        List<string> output = [];
+        List<long> executeTime = [];
+        bool isReturnVoid = methodInfo.ReturnType.FullName == "System.Void";
         while (loop-- > 0)
         {
             object[] input = new object[parameters.Length];
@@ -49,22 +50,24 @@
             {
                 Type type = parameters[i].ParameterType;
                 var inputFunc = InputMapper.GetValueOrDefault(type.FullName, s => s);
-                input[i] = inputFunc(data[index]);
+                input[i] = inputFunc(testcases[index]);
             }
             var watch = Stopwatch.StartNew();
             var result = methodInfo.Invoke(classInstance, input);
             watch.Stop();
             var elapsedMs = watch.ElapsedMilliseconds;
             var outputFunc = OutputMapper.GetValueOrDefault(methodInfo.ReturnType.FullName, JsonConvert.SerializeObject);
-            lines.Add((outputFunc(result), elapsedMs));
+
+            output.Add(outputFunc(isReturnVoid ? input[0] : result));
+            executeTime.Add(elapsedMs);
         }
 
-        Compare(lines);
+        CheckAnswer(output, executeTime, isReturnVoid);
     }
 
-    private static List<string> Input()
+    private static List<string> GetTestcase()
     {
-        using StreamReader sr = new("input.txt");
+        using StreamReader sr = new("testcase.txt");
         List<string> lines = [];
         var line = sr.ReadLine();
 
@@ -77,47 +80,42 @@
         return lines;
     }
 
-    private static void Output(List<string> lines)
+    private static List<string> GetAnswer()
     {
-        foreach (var line in lines)
-        {
-            Console.WriteLine(line);
-        }
-    }
-
-    private static void Compare(List<(string output, long executeTime)> output)
-    {
-        Console.Clear();
-        Console.ResetColor();
-
-        using StreamReader sr = new("output.txt");
-        List<string> expected = [];
+        using StreamReader sr = new("answer.txt");
+        List<string> lines = [];
         var line = sr.ReadLine();
 
         while (!string.IsNullOrWhiteSpace(line))
         {
-            expected.Add(line.Trim());
+            lines.Add(line.Trim());
             line = sr.ReadLine();
         }
-        List<(string output, long executeTime)> actual = output.Where(x => !string.IsNullOrEmpty(x.output)).ToList();
+        return lines;
+    }
 
-        if (expected.Count == 0)
+    private static void CheckAnswer(List<string> actual, List<long> executeTime, bool isReturnVoid)
+    {
+        Console.Clear();
+        Console.WriteLine("Test Results:");
+        List<string> expected = GetAnswer();
+
+        if (isReturnVoid)
         {
-            Output(actual.Select(x => x.output).ToList());
+            Console.WriteLine(string.Join("\n", actual));
             return;
         }
 
         if (expected.Count != actual.Count)
         {
-            Console.WriteLine("Output is not correct");
+            Console.WriteLine("Output is not correct!");
             return;
         }
 
         for (int i = 0; i < expected.Count; i++)
         {
-            bool isCorrect = string.Compare(expected[i], actual[i].output) == 0;
-            bool isTLE = actual[i].executeTime > 2000;
-            Console.ResetColor();
+            bool isCorrect = string.Compare(expected[i], actual[i]) == 0;
+            bool isTLE = executeTime[i] > 2000;
             if (isCorrect)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
@@ -128,13 +126,10 @@
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.Write("✘ ");
             }
-            Console.WriteLine($"Testcase {i + 1} ({actual[i].executeTime} ms)");
+            Console.WriteLine($"Testcase {i + 1} ({executeTime[i]} ms)");
             if (!isCorrect)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(actual[i].output);
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(expected[i]);
+                PrintDifferences(expected[i], actual[i]);
             }
             else if (isTLE)
             {
@@ -142,5 +137,27 @@
                 Console.WriteLine("Time Limit Exceeded");
             }
         }
+    }
+
+    private static void PrintDifferences(string expected, string actual)
+    {
+        int n = expected.Length, m = actual.Length;
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(expected);
+
+        for (int i = 0; i < m; i++)
+        {
+            if (i > n - 1 || expected[i] != actual[i])
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(actual[i]);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(actual[i]);
+            }
+        }
+        Console.WriteLine();
     }
 }
