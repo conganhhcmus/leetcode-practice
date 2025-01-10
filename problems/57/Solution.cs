82806fdf493e5a31f706988e3b87ef272f905404
conganhhcmus <conganhhcmus@gmail.com>
2025-01-10 17:00:02 +0700

    feat: add solution problem 57

---
+++ problems/57/Solution.cs
@@ -0,0 +1,32 @@
+#if DEBUG
+namespace Problems_57;
+#endif
+
+public class Solution
+{
+    public int[][] Insert(int[][] intervals, int[] newInterval)
+    {
+        List<int[]> ans = [];
+        foreach (var interval in intervals)
+        {
+            if (interval[1] < newInterval[0])
+            {
+                ans.Add(interval);
+            }
+            else if (interval[0] > newInterval[1])
+            {
+                ans.Add(newInterval);
+                newInterval = interval;
+            }
+            else
+            {
+                newInterval[0] = Math.Min(newInterval[0], interval[0]);
+                newInterval[1] = Math.Max(newInterval[1], interval[1]);
+            }
+        }
+
+        ans.Add(newInterval);
+
+        return [.. ans];
+    }
+}
