b69851b2634418cf0659303dcdcee3c47e4bf7a7
conganhhcmus <conganhhcmus@gmail.com>
2025-02-02 20:39:18 +0700

    feat: add solution problem 1752 (cont)

--- problems/1752/Solution.cs
+++ problems/1752/Solution.cs
@@ -6,28 +6,22 @@ public class Solution
 {
     public bool Check(int[] nums)
     {
-        int n = nums.Length;
-        int[] clone = [.. nums];
-        Array.Sort(clone);
-        List<int> posRotates = [];
-        for (int i = 0; i < n; i++)
+        int pos = -1, n = nums.Length;
+        if (n == 1) return true;
+        for (int i = 1; i < n; i++)
         {
-            if (nums[i] == clone[0]) posRotates.Add(i);
-        }
-        foreach (int pos in posRotates)
-        {
-            bool ans = true;
-            for (int i = 0; i < n; i++)
+            if (nums[i] < nums[i - 1])
             {
-                if (nums[(i + pos) % n] != clone[i])
-                {
-                    ans = false;
-                    break;
-                }
+                pos = i;
+                break;
             }
-            if (ans) return true;
         }
-
-        return false;
+        if (pos == -1) return true;
+        if (nums[pos] > nums[0]) return false;
+        for (int i = pos + 1; i < n; i++)
+        {
+            if (nums[i] < nums[i - 1] || nums[i] > nums[0]) return false;
+        }
+        return true;
     }
 }
