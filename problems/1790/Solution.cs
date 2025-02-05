3a4e6c4ace0262ee23717048ad336fcf4a402df5
conganhhcmus <conganhhcmus@gmail.com>
2025-02-05 11:14:19 +0700

    feat: add solution problem 1790

---
+++ problems/1790/Solution.cs
@@ -0,0 +1,28 @@
+#if DEBUG
+namespace Problems_1790;
+#endif
+
+public class Solution
+{
+    public bool AreAlmostEqual(string s1, string s2)
+    {
+        int n = s1.Length;
+        int[] freq = new int[26];
+        for (int i = 0; i < n; i++)
+        {
+            freq[s1[i] - 'a']++;
+            freq[s2[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++)
+        {
+            if (freq[i] != 0) return false;
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (s1[i] == s2[i]) continue;
+            count++;
+        }
+        return count == 2 || count == 0;
+    }
+}
