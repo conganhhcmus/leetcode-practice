f77849806f119ba2be193e33dbc369e2de28527d
conganhhcmus <conganhhcmus@gmail.com>
2025-02-06 10:50:05 +0700

    feat: add solution problem 112

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -1,4 +1,4 @@
-global using Running = Problems_1726_2;
+global using Running = Problems_112;
 global using Helpers;
 global using Newtonsoft.Json;
 global using System.Reflection;
---
+++ problems/112/Solution.cs
@@ -0,0 +1,26 @@
+#if DEBUG
+namespace Problems_112;
+#endif
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public bool HasPathSum(TreeNode root, int targetSum)
+    {
+        if (root == null) return false;
+        if (root.left == null && root.right == null && root.val == targetSum) return true;
+        return HasPathSum(root.left, targetSum - root.val) || HasPathSum(root.right, targetSum - root.val);
+    }
+}
