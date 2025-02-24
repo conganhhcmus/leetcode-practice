c0a085e0f67f36c63f986a269c43eb13657184fd
conganhhcmus <conganhhcmus@gmail.com>
2025-02-24 20:43:29 +0700

    feat: add solution problem 399 (cont)

--- problems/399/Solution.cs
+++ problems/399/Solution.cs
@@ -1,44 +1,54 @@
-namespace Problem_399;
+#if DEBUG
+namespace Problems_399;
+#endif
 
 public class Solution
 {
     public double[] CalcEquation(IList<IList<string>> equations, double[] values, IList<IList<string>> queries)
     {
-        Dictionary<string, List<(string des, double values)>> graph = [];
+        Dictionary<string, List<(string des, double val)>> graph = [];
 
         for (int i = 0; i < equations.Count; i++)
         {
-            var a = equations[i][0];
-            var b = equations[i][1];
-            graph[a] = graph.GetValueOrDefault(a, []);
+            string a = equations[i][0], b = equations[i][1];
+            graph.TryAdd(a, []);
+            graph.TryAdd(b, []);
             graph[a].Add((b, values[i]));
-            graph[b] = graph.GetValueOrDefault(b, []);
             graph[b].Add((a, 1 / values[i]));
-        }
-
-        double DFS(string curr, string end, double currentValue, HashSet<string> mapped)
-        {
-            if (curr == end) return currentValue;
-            if (!mapped.Add(curr)) return -1;
-
-            foreach (var (des, val) in graph[curr])
-            {
-                var ans = DFS(des, end, currentValue * val, mapped);
-                if (ans != -1) return ans;
-            }
-
-            return -1;
         }
 
         double[] result = new double[queries.Count];
         for (int i = 0; i < queries.Count; i++)
         {
-            var a = queries[i][0];
-            var b = queries[i][1];
-            if (!graph.ContainsKey(a) || !graph.ContainsKey(b)) result[i] = -1.0;
-            else if (a == b) result[i] = 1.0;
-            else result[i] = DFS(a, b, 1.0, []);
+            string c = queries[i][0], d = queries[i][1];
+            result[i] = GetResult(graph, c, d);
         }
         return result;
+    }
+
+    double GetResult(Dictionary<string, List<(string, double)>> graph, string c, string d)
+    {
+        if (!graph.ContainsKey(c) || !graph.ContainsKey(d)) return -1D;
+        if (c == d) return 1D;
+
+        Stack<(string, double)> stack = [];
+        HashSet<string> visited = [];
+        stack.Push((c, 1D));
+        visited.Add(c);
+        while (stack.Count > 0)
+        {
+            var (currNode, currVal) = stack.Pop();
+            if (currNode == d) return currVal;
+            bool isLeaf = true;
+            foreach (var (des, val) in graph[currNode])
+            {
+                if (!visited.Add(des)) continue;
+                isLeaf = false;
+                stack.Push((des, currVal * val));
+            }
+            if (isLeaf) visited.Remove(currNode);
+        }
+
+        return -1D;
     }
 }
