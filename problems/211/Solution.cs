579c383152fba7943ff76f234ce567e632924dea
conganhhcmus <conganhhcmus@gmail.com>
2025-03-04 10:16:39 +0700

    feat: add solution problem 211

---
+++ problems/211/Solution.cs
@@ -0,0 +1,99 @@
+#if DEBUG
+namespace Problems_211;
+#endif
+
+public class WordDictionary
+{
+    WordDictionary[] children;
+    bool isEndOfWord;
+
+    public WordDictionary()
+    {
+        children = new WordDictionary[26];
+        isEndOfWord = false;
+    }
+
+    public void AddWord(string word)
+    {
+        WordDictionary node = this;
+        foreach (char c in word)
+        {
+            int index = c - 'a';
+            if (node.children[index] == null)
+            {
+                node.children[index] = new WordDictionary();
+            }
+            node = node.children[index];
+        }
+        node.isEndOfWord = true;
+    }
+
+    public bool Search(string word)
+    {
+        WordDictionary node = this;
+        for (int i = 0; i < word.Length; i++)
+        {
+            char c = word[i];
+            if (c == '.')
+            {
+                for (int j = 0; j < 26; j++)
+                {
+                    WordDictionary next = node.children[j];
+                    if (next != null)
+                    {
+                        if (next.Search(word[(i + 1)..]))
+                        {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+
+            int index = c - 'a';
+            if (node.children[index] == null)
+            {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return node.isEndOfWord;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.AddWord(word);
+ * bool param_2 = obj.Search(word);
+ */
+
+
+public class Solution
+{
+    public List<dynamic> Execute(string[] actions, string[][] values)
+    {
+        List<dynamic> result = [];
+        WordDictionary wordDictionary = null;
+        for (int i = 0; i < actions.Length; i++)
+        {
+            switch (actions[i])
+            {
+                case "WordDictionary":
+                    wordDictionary = new WordDictionary();
+                    result.Add(null);
+                    break;
+                case "addWord":
+                    wordDictionary.AddWord(values[i][0]);
+                    result.Add(null);
+                    break;
+                case "search":
+                    result.Add(wordDictionary.Search(values[i][0]));
+                    break;
+                default:
+                    break;
+            }
+        }
+        return result;
+    }
+}
---
+++ problems/211/Solution2.cs
@@ -0,0 +1,86 @@
+#if DEBUG
+namespace Problems_211_2;
+#endif
+
+public class WordDictionary
+{
+    Dictionary<int, HashSet<string>> dictionary;
+
+    public WordDictionary()
+    {
+        dictionary = [];
+    }
+
+    public void AddWord(string word)
+    {
+        int key = word.Length;
+        dictionary.TryAdd(key, []);
+        dictionary[key].Add(word);
+    }
+
+    public bool Search(string word)
+    {
+        int key = word.Length;
+        if (dictionary.TryGetValue(key, out HashSet<string> candidates))
+        {
+            if (word.Contains('.'))
+            {
+                foreach (string candidate in candidates)
+                {
+                    bool isMatch = true;
+                    for (int i = 0; i < candidate.Length; i++)
+                    {
+                        if (word[i] == '.') continue;
+                        if (candidate[i] != word[i])
+                        {
+                            isMatch = false;
+                            break;
+                        }
+                    }
+
+                    if (isMatch) return true;
+                }
+            }
+            return candidates.Contains(word);
+        }
+
+        return false;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.AddWord(word);
+ * bool param_2 = obj.Search(word);
+ */
+
+
+public class Solution
+{
+    public List<dynamic> Execute(string[] actions, string[][] values)
+    {
+        List<dynamic> result = [];
+        WordDictionary wordDictionary = null;
+        for (int i = 0; i < actions.Length; i++)
+        {
+            switch (actions[i])
+            {
+                case "WordDictionary":
+                    wordDictionary = new WordDictionary();
+                    result.Add(null);
+                    break;
+                case "addWord":
+                    wordDictionary.AddWord(values[i][0]);
+                    result.Add(null);
+                    break;
+                case "search":
+                    result.Add(wordDictionary.Search(values[i][0]));
+                    break;
+                default:
+                    break;
+            }
+        }
+        return result;
+    }
+}
