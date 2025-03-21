dafdb52d8c54e0e701c9238018e89578e91aa522
conganhhcmus <conganhhcmus@gmail.com>
2025-03-21 21:29:47 +0700

    feat: add solution problem 190 (cont)

--- problems/191/Solution.cs
+++ problems/191/Solution.cs
@@ -1,16 +1,18 @@
+#if DEBUG
 namespace Problems_191;
+#endif
 
 public class Solution
 {
     public int HammingWeight(int n)
     {
-        int ans = 0;
+        int count = 0;
         while (n > 0)
         {
-            ans += n & 1;
+            count += n & 1;
             n >>= 1;
         }
 
-        return ans;
+        return count;
     }
 }
