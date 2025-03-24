e5927304f61217f764c3da9456f9a3cb3d14e9a7
conganhhcmus <conganhhcmus@gmail.com>
2025-03-24 17:17:02 +0700

    feat: add solution problem 17 (cont)

--- problems/17/Solution.cs
+++ problems/17/Solution.cs
@@ -1,41 +1,39 @@
-namespace Problem_17;
-
-using System.Text;
+#if DEBUG
+namespace Problems_17;
+#endif
 
 public class Solution
 {
     public IList<string> LetterCombinations(string digits)
     {
-        if (digits.Length == 0) return [];
-        var map = new Dictionary<char, string>{
-            {'2', "abc"},
-            {'3', "def"},
-            {'4', "ghi"},
-            {'5', "jkl"},
-            {'6', "mno"},
-            {'7', "pqrs"},
-            {'8', "tuv"},
-            {'9', "wxyz"},
+        var map = new Dictionary<char, List<string>>{
+            {'2',["a","b","c"]},
+            {'3',["d","e","f"]},
+            {'4',["g","h","i"]},
+            {'5',["j","k","l"]},
+            {'6',["m","n","o"]},
+            {'7',["p","q","r","s"]},
+            {'8',["t","u","v"]},
+            {'9',["w","x","y","z"]},
         };
-        List<string> result = [];
-        StringBuilder sb = new();
 
-        void BackTracking(int index)
+        IList<string> ret = [];
+        BackTracking(0, "", digits, ret, map);
+        return ret;
+    }
+
+    void BackTracking(int pos, string candidate, string digits, IList<string> ret, Dictionary<char, List<string>> map)
+    {
+        if (digits.Length == 0) return;
+        if (pos >= digits.Length)
         {
-            if (index == digits.Length)
-            {
-                result.Add(sb.ToString());
-                return;
-            }
-
-            foreach (char c in map[digits[index]])
-            {
-                sb.Append(c);
-                BackTracking(index + 1);
-                sb.Length--;
-            }
+            ret.Add(candidate);
+            return;
         }
-        BackTracking(0);
-        return result;
+
+        foreach (string letter in map[digits[pos]])
+        {
+            BackTracking(pos + 1, candidate + letter, digits, ret, map);
+        }
     }
 }
