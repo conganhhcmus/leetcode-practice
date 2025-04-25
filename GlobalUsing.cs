c585d997e758d6219fcd0f26c39376bf966446f1
conganhhcmus <conganhhcmus@gmail.com>
2025-04-25 23:28:57 +0700

    feat: add solution problem 2845

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -1,4 +1,4 @@
-global using Running = Weekly_445_Q3;
+global using Running = Problems_2845;
 #region Global Using Declarations
 global using System.Diagnostics;
 global using System.Reflection;
---
+++ problems/2845/Solution.cs
@@ -0,0 +1,29 @@
+#if DEBUG
+namespace Problems_2845;
+#endif
+
+public class Solution
+{
+    public long CountInterestingSubarrays(IList<int> nums, int modulo, int k)
+    {
+        int n = nums.Count;
+        int[] count = new int[n + 1];
+        for (int i = 0; i < n; i++)
+        {
+            count[i + 1] = count[i] + (nums[i] % modulo == k ? 1 : 0);
+        }
+        long ret = 0;
+        // each i count j where j < i  and (count[i] - count[j]) % modulo == k
+        // count[j] % modulo == count[i] % modulo - k
+        // count[j] % modulo == (count[i] - k) % modulo
+        // count[j] % modulo == (count[i] - k + modulo) % modulo // remove negative modulo
+        Dictionary<int, int> map = [];
+        for (int i = 0; i <= n; ++i)
+        {
+            ret += map.GetValueOrDefault((count[i] + modulo - k) % modulo, 0);
+            map[count[i] % modulo] = map.GetValueOrDefault(count[i] % modulo, 0) + 1;
+        }
+
+        return ret;
+    }
+}
