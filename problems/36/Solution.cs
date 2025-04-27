4d03c0b7d18730fb86adc43b40c187d7ac3a7754
conganhhcmus <conganhhcmus@gmail.com>
2025-04-28 01:01:34 +0700

    feat: add solution problem 36 (cont)

--- problems/36/Solution.cs
+++ problems/36/Solution.cs
@@ -1,13 +1,10 @@
-namespace Problem_36;
+#if DEBUG
+namespace Problems_36;
+#endif
 
 public class Solution
 {
     public bool IsValidSudoku(char[][] board)
-    {
-        return IsValidSudoku_BitMask(board);
-    }
-
-    public bool IsValidSudoku_Loop(char[][] board)
     {
         // check row
         for (int i = 0; i < 9; i++)
@@ -46,34 +43,6 @@ public class Solution
                     }
                 }
 
-            }
-        }
-        return true;
-    }
-
-    public bool IsValidSudoku_BitMask(char[][] board)
-    {
-        int[] rows = new int[9];
-        int[] cols = new int[9];
-        int[] squares = new int[9];
-
-        for (int r = 0; r < 9; r++)
-        {
-            for (int c = 0; c < 9; c++)
-            {
-                if (board[r][c] == '.') continue;
-
-                int val = board[r][c] - '1';
-
-                if ((rows[r] & (1 << val)) > 0 || (cols[c] & (1 << val)) > 0 ||
-                    (squares[(r / 3) * 3 + (c / 3)] & (1 << val)) > 0)
-                {
-                    return false;
-                }
-
-                rows[r] |= (1 << val);
-                cols[c] |= (1 << val);
-                squares[(r / 3) * 3 + (c / 3)] |= (1 << val);
             }
         }
         return true;
