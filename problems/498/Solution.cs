d2d2fc6d69ee8304efc3ba4993a0806bd4e46856
conganhhcmus <conganhhcmus@gmail.com>
2025-04-27 15:16:07 +0700

    feat: add solution problem 498

---
+++ problems/498/Solution.cs
@@ -0,0 +1,43 @@
+#if DEBUG
+namespace Problems_498;
+#endif
+
+public class Solution
+{
+    public int[] FindDiagonalOrder(int[][] mat)
+    {
+        int n = mat.Length, m = mat[0].Length;
+        List<int>[] diag = new List<int>[n + m];
+        for (int i = 0; i < n + m; i++)
+        {
+            diag[i] = [];
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                diag[i + j].Add(mat[i][j]);
+            }
+        }
+        int[] ret = new int[n * m];
+        int idx = 0;
+        for (int i = 0; i < n + m; i++)
+        {
+            if (i % 2 != 0)
+            {
+                for (int j = 0; j < diag[i].Count; j++)
+                {
+                    ret[idx++] = diag[i][j];
+                }
+            }
+            else
+            {
+                for (int j = diag[i].Count - 1; j >= 0; j--)
+                {
+                    ret[idx++] = diag[i][j];
+                }
+            }
+        }
+        return ret;
+    }
+}
---
+++ problems/498/Solution2.cs
@@ -0,0 +1,61 @@
+#if DEBUG
+namespace Problems_498_2;
+#endif
+
+public class Solution
+{
+    public int[] FindDiagonalOrder(int[][] mat)
+    {
+        int n = mat.Length, m = mat[0].Length;
+        int[] ret = new int[n * m];
+        int idx = 0;
+        bool up = false;
+        int rowIdx = 0, colIdx = 0;
+        while (idx < n * m)
+        {
+            ret[idx] = mat[rowIdx][colIdx];
+            idx++;
+            if (!up)
+            {
+                if (rowIdx > 0 && colIdx < m - 1)
+                {
+                    rowIdx--;
+                    colIdx++;
+                }
+                else
+                {
+                    if (colIdx == m - 1)
+                    {
+                        rowIdx++;
+                    }
+                    else
+                    {
+                        colIdx++;
+                    }
+                    up = !up;
+                }
+            }
+            else
+            {
+                if (colIdx > 0 && rowIdx < n - 1)
+                {
+                    colIdx--;
+                    rowIdx++;
+                }
+                else
+                {
+                    if (rowIdx == n - 1)
+                    {
+                        colIdx++;
+                    }
+                    else
+                    {
+                        rowIdx++;
+                    }
+                    up = !up;
+                }
+            }
+        }
+        return ret;
+    }
+}
