dedb49d957ca100a2547e34953ede117dbef2fb0
conganhhcmus <conganhhcmus@gmail.com>
2025-05-06 22:23:05 +0700

    feat: add solution problem 376 (cont)

---
+++ problems/376/Solution2.cs
@@ -0,0 +1,33 @@
+#if DEBUG
+namespace Problems_376_2;
+#endif
+
+public class Solution
+{
+    public int WiggleMaxLength(int[] nums)
+    {
+        int n = nums.Length;
+        Pair[] dp = new Pair[n];
+        dp[0] = new(1, 1);
+        for (int i = 1; i < n; i++)
+        {
+            var prev = dp[i - 1];
+            if (nums[i] == nums[i - 1])
+            {
+                dp[i] = new(prev.Up, prev.Down);
+            }
+            else if (nums[i] > nums[i - 1])
+            {
+                dp[i] = new(Math.Max(prev.Up, prev.Down + 1), prev.Down);
+            }
+            else
+            {
+                dp[i] = new(prev.Up, Math.Max(prev.Down, prev.Up + 1));
+            }
+        }
+
+        return Math.Max(dp[n - 1].Up, dp[n - 1].Down);
+    }
+
+    public record Pair(int Up, int Down);
+}
