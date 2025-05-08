c5562ac3bbd5745f4795704c80f810e157b47ea9
conganhhcmus <conganhhcmus@gmail.com>
2025-05-08 10:30:47 +0700

    feat: add solution problem 3342

--- README.md
+++ README.md
@@ -759,6 +759,7 @@ This repository contains my solutions to various LeetCode problems and contests.
 - [3277. Maximum XOR Score Subarray Queries](./problems/3277/Solution.cs)
 - [3306. Count of Substrings Containing Every Vowel and K Consonants II](./problems/3306/Solution.cs)
 - [3341. Find Minimum Time to Reach Last Room I](./problems/3341/Solution.cs)
+- [3342. Find Minimum Time to Reach Last Room II](./problems/3342/Solution.cs)
 - [3356. Zero Array Transformation II](./problems/3356/Solution.cs)
 - [3364. Minimum Positive Sum Subarray](./problems/3364/Solution.cs)
 - [3365. Rearrange K Substrings to Form Target String](./problems/3365/Solution.cs)
---
+++ problems/3342/Solution.cs
@@ -0,0 +1,52 @@
+#if DEBUG
+using System.Runtime.Intrinsics.Arm;
+
+namespace Problems_3342;
+#endif
+
+public class Solution
+{
+    public int MinTimeToReach(int[][] moveTime)
+    {
+        int n = moveTime.Length, m = moveTime[0].Length;
+        int[][] minTime = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            minTime[i] = new int[m];
+            Array.Fill(minTime[i], int.MaxValue);
+        }
+
+        minTime[0][0] = 0;
+        PriorityQueue<Pair, int> pq = new();
+        pq.Enqueue(new(0, 0, 1), 0);
+        int[] dirs = [1, 0, -1, 0, 1];
+        while (pq.Count > 0)
+        {
+            Pair curr = pq.Dequeue();
+            for (int i = 0; i < 4; i++)
+            {
+                int newR = curr.Row + dirs[i];
+                int newC = curr.Col + dirs[i + 1];
+                int newAdjust = curr.Adjust ^ 1 ^ 2;
+                if (IsValid(newR, newC, n, m))
+                {
+                    int val = Math.Max(minTime[curr.Row][curr.Col], moveTime[newR][newC]) + curr.Adjust;
+                    if (val < minTime[newR][newC])
+                    {
+                        minTime[newR][newC] = val;
+                        pq.Enqueue(new(newR, newC, newAdjust), val);
+                    }
+                }
+            }
+        }
+
+        return minTime[n - 1][m - 1];
+    }
+
+    public record Pair(int Row, int Col, int Adjust);
+
+    bool IsValid(int r, int c, int n, int m)
+    {
+        return r >= 0 && r < n && c >= 0 && c < m;
+    }
+}
