13d7bea72e61ca87481f0b3d11ceecfe25b4bcef
conganhhcmus <conganhhcmus@gmail.com>
2025-05-12 23:54:11 +0700

    feat: add solution problem 455

---
+++ problems/455/Solution.cs
@@ -0,0 +1,22 @@
+#if DEBUG
+namespace Problems_455;
+#endif
+
+public class Solution
+{
+    public int FindContentChildren(int[] g, int[] s)
+    {
+        Array.Sort(g);
+        Array.Sort(s);
+        int ret = 0;
+        for (int i = 0, j = 0; i < s.Length && j < g.Length; i++)
+        {
+            if (s[i] >= g[j])
+            {
+                ret++;
+                j++;
+            }
+        }
+        return ret;
+    }
+}
