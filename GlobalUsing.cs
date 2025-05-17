7a578163c83b67c1a038b6f29a6ed8d015d1ecc9
conganhhcmus <conganhhcmus@gmail.com>
2025-05-17 13:57:23 +0700

    feat: add solution problem 1004 (cont)

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -1,4 +1,4 @@
-global using Running = Problems_2901_2;
+global using Running = Problems_1004_2;
 #region Global Using Declarations
 global using System.Diagnostics;
 global using System.Reflection;
---
+++ problems/1004/Solution2.cs
@@ -0,0 +1,41 @@
+#if DEBUG
+namespace Problems_1004_2;
+#endif
+
+public class Solution
+{
+    public int LongestOnes(int[] nums, int k)
+    {
+        int n = nums.Length;
+        int[] prefixCount = new int[n + 1];
+        for (int i = 1; i <= n; ++i)
+        {
+            prefixCount[i] = prefixCount[i - 1] + (1 - nums[i - 1]);
+        }
+        int low = 0, high = n, ans = 0;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if (Ok(prefixCount, mid, k))
+            {
+                ans = mid;
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    bool Ok(int[] prefixCount, int len, int k)
+    {
+        int n = prefixCount.Length;
+        for (int i = len; i < n; i++)
+        {
+            if (prefixCount[i] - prefixCount[i - len] <= k) return true;
+        }
+        return false;
+    }
+}
