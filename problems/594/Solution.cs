c5788a15fcfe86da74bb8b2da237e81e28d145b1
conganhhcmus <conganhhcmus@gmail.com>
2025-05-20 22:05:00 +0700

    feat: add solution problem 594

---
+++ problems/594/Solution.cs
@@ -0,0 +1,21 @@
+#if DEBUG
+namespace Problems_594;
+#endif
+
+public class Solution
+{
+    public int FindLHS(int[] nums)
+    {
+        Dictionary<int, int> count = [];
+        foreach (int num in nums)
+        {
+            count[num] = count.GetValueOrDefault(num, 0) + 1;
+        }
+        int ret = 0;
+        foreach (int num in count.Keys)
+        {
+            ret = Math.Max(ret, count[num] + count.GetValueOrDefault(num - 1, int.MinValue));
+        }
+        return ret;
+    }
+}
