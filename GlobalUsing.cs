935ed28e7820ed1db159b4dd7536116dccdd2e48
conganhhcmus <conganhhcmus@gmail.com>
2025-05-24 14:27:11 +0700

    feat: add solution problem 2942

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -1,4 +1,4 @@
-global using Running = Problems_3068_3;
+global using Running = Problems_2942;
 #region Global Using Declarations
 global using System.Diagnostics;
 global using System.Reflection;
---
+++ problems/2942/Solution.cs
@@ -0,0 +1,23 @@
+#if DEBUG
+namespace Problems_2942;
+#endif
+
+public class Solution
+{
+    public IList<int> FindWordsContaining(string[] words, char x)
+    {
+        List<int> ret = [];
+        for (int i = 0; i < words.Length; i++)
+        {
+            for (int j = 0; j < words[i].Length; j++)
+            {
+                if (words[i][j] == x)
+                {
+                    ret.Add(i);
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+}
