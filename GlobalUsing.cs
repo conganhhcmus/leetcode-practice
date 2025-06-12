1eb9007f21e91725e9557b28c2e49c224b817d8a
conganhhcmus <conganhhcmus@gmail.com>
2025-06-12 10:18:25 +0700

    feat: add solution problem 3423

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -1,4 +1,4 @@
-global using Running = Problems_440;
+global using Running = Problems_3423;
 #region Global Using Declarations
 global using System.Diagnostics;
 global using System.Reflection;
---
+++ problems/3423/Solution.cs
@@ -0,0 +1,17 @@
+#if DEBUG
+namespace Problems_3423;
+#endif
+
+public class Solution
+{
+    public int MaxAdjacentDistance(int[] nums)
+    {
+        int n = nums.Length;
+        int ret = Math.Abs(nums[0] - nums[n - 1]);
+        for (int i = 1; i < n; i++)
+        {
+            ret = Math.Max(ret, Math.Abs(nums[i] - nums[i - 1]));
+        }
+        return ret;
+    }
+}
