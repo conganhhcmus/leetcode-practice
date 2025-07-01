f32c097042c52903c1c24e11bb48efd11ab86ab0
conganhhcmus <conganhhcmus@gmail.com>
2025-07-01 09:49:29 +0700

    feat: add solution problem 3330

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -1,4 +1,4 @@
-global using Running = Problems_1498;
+global using Running = Problems_3330;
 #region Global Using Declarations
 global using System.Diagnostics;
 global using System.Reflection;
---
+++ problems/3330/Solution.cs
@@ -0,0 +1,20 @@
+#if DEBUG
+namespace Problems_3330;
+#endif
+
+public class Solution
+{
+    public int PossibleStringCount(string word)
+    {
+        int n = word.Length;
+        int ans = 1;
+        for (int i = 1; i < n; i++)
+        {
+            if (word[i] == word[i - 1])
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
