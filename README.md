c6b8426317bb6bef96b5684c58b2890eec2fd5ad
conganhhcmus <conganhhcmus@gmail.com>
2025-08-17 21:58:13 +0700

    feat: add solution problem 837

--- README.md
+++ README.md
@@ -502,6 +502,7 @@ This repository contains my solutions to various LeetCode problems and contests.
 - [802. Find Eventual Safe States](./problems/802/Solution.cs)
 - [808. Soup Servings](./problems/808/Solution.cs)
 - [827. Making A Large Island](./problems/827/Solution.cs)
+- [837. New 21 Game](./problems/837/Solution.cs)
 - [838. Push Dominoes](./problems/838/Solution.cs)
 - [841. Keys and Rooms](./problems/841/Solution.cs)
 - [847. Shortest Path Visiting All Nodes](./problems/847/Solution.cs)
---
+++ problems/837/Solution.cs
@@ -0,0 +1,31 @@
+#if DEBUG
+namespace Problems_837;
+#endif
+
+public class Solution
+{
+    public double New21Game(int n, int k, int maxPts)
+    {
+        double[] dp = new double[n + 1];
+        dp[0] = 1;
+        double s = k > 0 ? 1 : 0;
+        for (int i = 1; i <= n; i++)
+        {
+            dp[i] = s / maxPts;
+            if (i < k)
+            {
+                s += dp[i];
+            }
+            if (i - maxPts >= 0 && i - maxPts < k)
+            {
+                s -= dp[i - maxPts];
+            }
+        }
+        double ans = 0;
+        for (int i = k; i <= n; i++)
+        {
+            ans += dp[i];
+        }
+        return ans;
+    }
+}
