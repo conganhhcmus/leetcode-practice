6ec6fe6004a404b07c5a4de3f30ca645e6bc6774
conganhhcmus <conganhhcmus@gmail.com>
2025-10-14 13:39:45 +0700

    feat: add solution problem 3349

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -1,4 +1,4 @@
-global using Running = Problems_2273;
+global using Running = Problems_3349;
 #region Global Using Declarations
 global using System.Diagnostics;
 global using System.Reflection;
---
+++ problems/3349/Solution.cs
@@ -0,0 +1,27 @@
+#if DEBUG
+namespace Problems_3349;
+#endif
+
+public class Solution
+{
+    public bool HasIncreasingSubarrays(IList<int> nums, int k)
+    {
+        int n = nums.Count;
+        int need = k - 1;
+        if (need == 0) return true;
+        for (int i = k + 1; i < n; i++)
+        {
+            if (nums[i] > nums[i - 1] && nums[i - k] > nums[i - k - 1])
+            {
+                need--;
+            }
+            else
+            {
+                need = k - 1;
+            }
+
+            if (need == 0) return true;
+        }
+        return false;
+    }
+}
