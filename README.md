167ff2d585a7a574822dd2546d507160774a90e8
conganhhcmus <conganhhcmus@gmail.com>
2025-10-27 22:24:16 +0700

    feat: add solution problem 269

--- README.md
+++ README.md
@@ -391,6 +391,7 @@ This repository contains my solutions to various LeetCode problems and contests.
 - [241. Different Ways to Add Parentheses](./problems/241/Solution.cs)
 - [242. Valid Anagram](./problems/242/Solution.cs)
 - [264. Ugly Number II](./problems/264/Solution.cs)
+- [269. Alien Dictionary](./problems/269/Solution.cs)
 - [274. H-Index](./problems/274/Solution.cs)
 - [278. First Bad Version](./problems/278/Solution.cs)
 - [279. Perfect Squares](./problems/279/Solution.cs)
---
+++ problems/269/Solution.cs
@@ -0,0 +1,72 @@
+#if DEBUG
+namespace Problems_269;
+#endif
+
+public class Solution
+{
+    public string foreignDictionary(string[] words)
+    {
+        List<int>[] adj = new List<int>[26];
+        for (int i = 0; i < 26; i++)
+        {
+            adj[i] = [];
+        }
+        int n = words.Length;
+        if (n == 1) return words[0];
+        Dictionary<int, int> inDegree = [];
+        for (int i = 0; i < n; i++)
+        {
+            foreach (char ch in words[i])
+            {
+                inDegree.TryAdd(ch - 'a', 0);
+            }
+        }
+        for (int i = 1; i < n; i++)
+        {
+            string a = words[i - 1];
+            string b = words[i];
+            bool foundDiff = false;
+            for (int j = 0; j < Math.Min(a.Length, b.Length); j++)
+            {
+                if (a[j] != b[j])
+                {
+                    adj[a[j] - 'a'].Add(b[j] - 'a');
+                    inDegree[b[j] - 'a']++;
+                    foundDiff = true;
+                    break;
+                }
+            }
+            if (!foundDiff && a.Length > b.Length) return "";
+        }
+
+        // Topological sort
+        Queue<int> queue = [];
+        foreach (var pair in inDegree)
+        {
+            if (pair.Value == 0) queue.Enqueue(pair.Key);
+        }
+        StringBuilder ans = new();
+
+        while (queue.Count > 0)
+        {
+            int curr = queue.Dequeue();
+            char ch = (char)('a' + curr);
+            ans.Append(ch);
+            foreach (int neighbor in adj[curr])
+            {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0)
+                {
+                    queue.Enqueue(neighbor);
+                }
+            }
+        }
+
+        foreach (var pair in inDegree)
+        {
+            // has cycle
+            if (pair.Value != 0) return "";
+        }
+        return ans.ToString();
+    }
+}
