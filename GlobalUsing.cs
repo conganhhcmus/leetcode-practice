d8cf3ce67de8ebe4ccf102b7f7f42bf5d90e519c
conganhhcmus <conganhhcmus@gmail.com>
2025-11-18 00:18:13 +0700

    feat: add solution problem 7

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -1,4 +1,4 @@
-global using Running = Problems_2485_2;
+global using Running = Problems_7_2;
 #region Global Using Declarations
 global using System.Diagnostics;
 global using System.Reflection;
---
+++ problems/7/Solution2.cs
@@ -0,0 +1,20 @@
+#if DEBUG
+namespace Problems_7_2;
+#endif
+
+public class Solution
+{
+    public int Reverse(int x)
+    {
+        int ans = 0;
+        while (x != 0)
+        {
+            int digit = x % 10;
+            if (ans > int.MaxValue / 10) return 0;
+            if (ans < int.MinValue / 10) return 0;
+            ans = ans * 10 + digit;
+            x /= 10;
+        }
+        return ans;
+    }
+}
