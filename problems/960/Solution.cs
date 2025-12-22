e525485711a1be1ccf7a997f5df08f0f7de4bca5
conganhhcmus <conganhhcmus@gmail.com>
2025-12-22 22:11:41 +0700

    feat: add solution problem 960

---
+++ problems/960/Solution.cs
@@ -0,0 +1,42 @@
+#if DEBUG
+namespace Problems_960;
+#endif
+
+public class Solution
+{
+    public int MinDeletionSize(string[] strs)
+    {
+        return DP(0, -1, strs);
+    }
+
+    Dictionary<(int, int), int> memo = [];
+
+    int DP(int pos, int prev, string[] strs)
+    {
+        int n = strs.Length, m = strs[0].Length;
+        if (pos >= m) return 0;
+        var key = (pos, prev);
+        if (memo.TryGetValue(key, out int cache)) return cache;
+        int ans = int.MaxValue;
+        // check each row
+        bool isCorrect = true;
+        for (int i = 0; i < n; i++)
+        {
+            if (prev >= 0 && strs[i][pos] < strs[i][prev])
+            {
+                isCorrect = false;
+                break;
+            }
+        }
+        if (isCorrect)
+        {
+            ans = Math.Min(DP(pos + 1, prev, strs) + 1, DP(pos + 1, pos, strs));
+        }
+        else
+        {
+            ans = DP(pos + 1, prev, strs) + 1;
+        }
+
+        return memo[key] = ans;
+    }
+}
---
+++ problems/960/Solution2.cs
@@ -0,0 +1,34 @@
+#if DEBUG
+namespace Problems_960_2;
+#endif
+
+public class Solution
+{
+    public int MinDeletionSize(string[] strs)
+    {
+        int n = strs.Length, m = strs[0].Length;
+        int[] dp = new int[m];
+        int best = 1;
+
+        for (int j = 0; j < m; j++)
+        {
+            dp[j] = 1;
+            for (int k = 0; k < j; k++)
+            {
+                bool ok = true;
+                for (int i = 0; i < n; i++)
+                {
+                    if (strs[i][k] > strs[i][j])
+                    {
+                        ok = false;
+                        break;
+                    }
+                }
+                if (ok) dp[j] = Math.Max(dp[j], dp[k] + 1);
+            }
+            best = Math.Max(best, dp[j]);
+        }
+
+        return m - best;
+    }
+}
