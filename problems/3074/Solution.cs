419794481462042e96733f053dd266c3d2ede19d
conganhhcmus <conganhhcmus@gmail.com>
2025-12-24 17:40:27 +0700

    feat: add solution problem 3074

---
+++ problems/3074/Solution.cs
@@ -0,0 +1,25 @@
+#if DEBUG
+namespace Problems_3074;
+#endif
+
+public class Solution
+{
+    public int MinimumBoxes(int[] apple, int[] capacity)
+    {
+        int sum = 0;
+        foreach (int num in apple)
+        {
+            sum += num;
+        }
+        Array.Sort(capacity);
+        int ans = 0;
+        for (int i = capacity.Length - 1; i >= 0; i--)
+        {
+            if (sum <= 0) break;
+            ans++;
+            sum -= capacity[i];
+        }
+
+        return ans;
+    }
+}
