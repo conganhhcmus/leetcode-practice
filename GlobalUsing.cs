9cbf015e94e3631d59980311316fbc289182db24
conganhhcmus <conganhhcmus@gmail.com>
2025-12-29 12:57:12 +0700

    feat: add solution problem 756

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -1,4 +1,4 @@
-global using Running = Problems_1351_2;
+global using Running = Problems_756;
 #region Global Using Declarations
 global using System.Diagnostics;
 global using System.Reflection;
---
+++ problems/756/Solution.cs
@@ -0,0 +1,48 @@
+#if DEBUG
+namespace Problems_756;
+#endif
+
+public class Solution
+{
+    public bool PyramidTransition(string bottom, IList<string> allowed)
+    {
+        Dictionary<string, List<char>> map = [];
+        foreach (string str in allowed)
+        {
+            string key = str[..2];
+            char val = str[2];
+            map.TryAdd(key, []);
+            map[key].Add(val);
+        }
+        return BuildPyramid("", bottom, map);
+    }
+
+    Dictionary<string, bool> memo = [];
+
+    bool BuildPyramid(string cur, string bottom, Dictionary<string, List<char>> map)
+    {
+        if (bottom.Length == 1) return true;
+
+        if (cur.Length == 0 && memo.TryGetValue(bottom, out bool cached))
+            return cached;
+
+        int i = cur.Length;
+
+        if (i == bottom.Length - 1)
+        {
+            bool res = BuildPyramid("", cur, map);
+            memo[bottom] = res;
+            return res;
+        }
+
+        string key = "" + bottom[i] + bottom[i + 1];
+        if (!map.ContainsKey(key)) return false;
+
+        foreach (char c in map[key])
+        {
+            if (BuildPyramid(cur + c, bottom, map)) return true;
+        }
+
+        return false;
+    }
+}
